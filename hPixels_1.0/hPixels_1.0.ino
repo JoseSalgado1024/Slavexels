58fab548e30504b809c510802ddbdad020316690
J. Salgado <JoseSalgado@MacBook-Pro-de-Jose.local>
2014-10-30 18:59:56 -0300

    RGB mod 1
    Fades entre las diferentes posiciones de la tabla de acciones.

--- hPixels_1.0/hPixels_1.0.ino
+++ hPixels_1.0/hPixels_1.0.ino
@@ -1,17 +1,37 @@
 #include <Flash.h>
-
+#include <hPix_conf.h>
 #include <Thread.h>
 #include <ThreadController.h>
 
 #include <Event.h>
 #include <Timer.h>
 
-#define realTime 10
 
-#define prH1   realTime*10
-#define prH2   realTime*25
-#define prL1   realTime*50
-#define prL2   realTime*100
+
+#define realTime 10
+#define prH1     50
+#define prH2     100
+#define prL1     250
+#define prL2     500
+
+#define cSteps 5
+
+//LED RGB PINS
+#define   RED 3
+#define GREEN 6
+#define  BLUE 5
+
+
+
+FLASH_TABLE(byte, act_table, 4 
+/*| R  |  G  |  B  | SND  |*/, 
+{
+  0x00, 0x00, 0x00, 0x00}
+, 
+{
+  0x00, 0x00, 0x00, 0x00}
+);
+
 
 
 ThreadController controll = ThreadController();
@@ -20,13 +40,19 @@ Thread* radThread     = new Thread();
 Thread  rgbThread     = Thread();
 Thread  soundThread   = Thread();
 
+
+int aPntr = 0;
+int aPass = 0;
+int r, g, b;
+
+
 void setup(){
   radThread->onRun(radThreadCallBack);
   radThread->setInterval(prH1);
 
   rgbThread.onRun(rgbThreadCallBack);
-  rgbThread.setInterval(prH2);
-  
+  rgbThread.setInterval(prL1);
+
   soundThread.onRun(soundThreadCallBack);
   soundThread.setInterval(prL1);
 
@@ -37,12 +63,53 @@ void setup(){
 
 
 void loop(){
+  controll.run();
 }
 
 void rgbThreadCallBack(){
+  if (aPntr < act_table.rows()) {
+    if (aPass < cSteps) {
+      //FADES BETWEEN COLOR ACTUAL RGB AND NEXT RGB
+      // RED COLOR
+      r = act_table[aPntr][0]-(act_table[aPntr][0]-act_table[aPntr+1][0])/cSteps*aPass;
+      // GREEN COLOR
+      g = act_table[aPntr][1]-(act_table[aPntr][1]-act_table[aPntr+1][1])/cSteps*aPass;
+      // BLUE COLOR
+      b = act_table[aPntr][2]-(act_table[aPntr][2]-act_table[aPntr+1][2])/cSteps*aPass;
+      aPass++;
+      paintLeds(r,g,b);
+    } 
+    else {
+      aPass=0;
+      aPntr++;
+      paintLeds(act_table[aPntr][0], act_table[aPntr][1], act_table[aPntr][2]);
+    }
+
+  }
 }
+
+
+void paintLeds(int _r, int _g, int _b){
+  analogWrite(GREEN,255- r);
+  analogWrite(BLUE, 255-g);
+  analogWrite(RED,255- b);
+}
+
+void rgb_setup()
+{
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  pinMode(RED, OUTPUT);
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(BLUE, HIGH);
+  digitalWrite(RED, HIGH);
+}
+
 void soundThreadCallBack(){
 }
 void radThreadCallBack(){
 }
+
+
+
 
